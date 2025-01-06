9db769115593312f0564fda0f2ec75b0f5ca7302
egarra <garikhabuzov@gmail.com>
2025-01-06 13:55:29 +0200

    fix: update styles

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const ButtonLoadMore = styled.button `
+export const ButtonLoadMore = styled.button`
   margin: 20px auto;
   padding: 8px 16px;
   border-radius: 2px;
@@ -21,11 +21,11 @@ export const ButtonLoadMore = styled.button `
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
 
-    :hover,
-    :focus {
-        background-color: #303f9f;
-    }
-`
-export const Wrapper = styled.div `
-    width: 1536px;
-`
+  :hover,
+  :focus {
+    background-color: #303f9f;
+  }
+`;
+export const Wrapper = styled.div`
+  width: 100%;
+`;
